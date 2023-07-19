b3ea21f100724e80f07e24c75e9a67903bbdd997
Ilia Tsarev <bkilja@gmail.com>
2023-07-19 11:59:36 +0400

    init

---
+++ src/BmiService.java
@@ -0,0 +1,9 @@
+public class BmiService {
+    public int calculate(double heightMeters, double weightKg) {
+
+        double index = weightKg / (heightMeters * heightMeters);
+
+        return (int)index;
+    }
+
+}
---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        BmiService service = new BmiService();
+        double heightMeters = 1.87;
+        double weightKg = 98;
+
+        int index = service.calculate(heightMeters, weightKg);
+
+        System.out.println("BMI: " + index);
+
+    }
+}
